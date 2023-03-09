5b61e2863adbea14c848cf677eda70b6f15e965f
Aditya Nandwana <you@example.com>
2023-03-10 00:12:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors=require("cors");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+const userRoutes = require("./routes/userRoutes")
+const adminRoutes = require("./routes/adminRoutes")
+mongoose.set('strictQuery', true);
+
+try {
+  mongoose.connect(process.env.MONGO_URI);   
+  console.log("DB Connected");
+} catch (err) {
+  console.log(err);
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.use(express.static(`${__dirname}/build/static`))
+
+app.get('/',(req,res)=>{
+res.sendFile(`${__dirname}/build/index.html`)
+})
+
+
+app.listen(5000, () => {
+  console.log("Backend Server running ");
+});
